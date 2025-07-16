d3f69a5c5d78096909ab2c92f58aaf208391aca5
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-07-16 16:38:28 +0200

    using useContext to prvent prop drilling

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useContext} from "react";
 
 export default function UserProfile(props){
+    const userData = useContext(UserContext)
     return (
         <div style={{border: '5px solid gray', padding: '10px', margin: '10px'}}>
             <h2 style={{color: 'lightblue'}}>{props.name}</h2>

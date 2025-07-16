f5dfe33c42a85a25e457666cdc1225ac22b73dab
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-07-16 15:53:10 +0200

    used useState for counter increment andddddddddddd decrement

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +19,7 @@ function App() {
     <MainContent />
     <Footer />
     <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    <Counter />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />

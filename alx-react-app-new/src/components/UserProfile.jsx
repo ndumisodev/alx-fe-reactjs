bef01bb9fc9d4ac8d841eac157b1295ed21793ce
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-07-16 15:25:50 +0200

    Addindind : duplicated alx react app

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function UserProfile(props){
     return (
-        <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-             <p>Bio: {props.bio}</p>
+        <div style={{border: '5px solid gray', padding: '10px', margin: '10px'}}>
+            <h2 style={{color: 'lightblue'}}>{props.name}</h2>
+            <p>Age: <span style={{fontWeight:'bold'}}> {props.age}</span></p>
+            <p>Bio: {props.bio}</p>
         </div>
     )
 }
